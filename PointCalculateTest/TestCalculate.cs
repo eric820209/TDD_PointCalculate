b31632cf0278680bf6d4c95117fe23bb85bd175a
Alan.Lin <eric820209@gmail.com>
2020-09-23 11:44:28 +0800

    拿掉一開始過於簡單的測試(分數只有分數沒有model

--- PointCalculateTest/TestCalculate.cs
+++ PointCalculateTest/TestCalculate.cs
@@ -13,46 +13,22 @@ namespace PointCalculator_Test
 {
     public class Tests
     {
-        private PointPiorityQueue _pointPiorityQueue;
         private ModelBuilder _modelBuilder;
         [SetUp]
         public void Setup()
         {
-            Mock<IDateTimeService> mock_DateTimeService = new Mock<IDateTimeService>();
-            mock_DateTimeService.Setup(x => x.GetDate()).Returns(DateTime.Parse("2020/03/12"));
-
-            _pointPiorityQueue = new PointPiorityQueue(mock_DateTimeService.Object);
             _modelBuilder = new ModelBuilder();
         }
 
         [Test]
-        [TestCase("1")]
-        [TestCase("10.52")]
-        [TestCase("-20.56")]
-        public void Calculate_OnePointTransactionDetailRecord_ReturnSpecifyPoint(decimal points)
-        {
-            ///Arrange
-            List<PointTransactionDetail> transactions = new List<PointTransactionDetail>
-            {
-            _modelBuilder.CreateTransactionDetail(points),
-            };
-            ///Action
-            var actual = _pointPiorityQueue.Calculate(transactions);
-
-            ///Assert
-            List<PointTransactionDetail> expect = new List<PointTransactionDetail>
-            {
-            _modelBuilder.CreateTransactionDetail(points),
-            };
-
-            Assert.AreEqual(JsonSerializer.Serialize(expect), JsonSerializer.Serialize(actual));
-        }
-
-        [Test]
         [TestCaseSource(typeof(MultiplePositivePoints_TestCaseSource))]
-        public void Calculate_MultiplePositiveRecord_OrderByExpireDate(TestCaseExpectAndActual caseDetail )
+        public void Calculate_MultiplePositiveRecord_OrderByExpireDate(TestCaseExpectAndActualModel caseDetail )
         {
             ///Arrange
+            //設定截止日，因為截止日跟test case掛勾，無法在set up 設定
+            Mock<IDateTimeService> mock_DateTimeService = new Mock<IDateTimeService>();
+            mock_DateTimeService.Setup(x => x.GetDate()).Returns(caseDetail.MockGetPointDate);
+            PointPiorityQueue _pointPiorityQueue = new PointPiorityQueue(mock_DateTimeService.Object);
 
             ///Action
             var actual = _pointPiorityQueue.Calculate(caseDetail.funcInput);
@@ -66,10 +42,13 @@ namespace PointCalculator_Test
 
         [Test]
         [TestCaseSource(typeof(MultipleNegativePoints_TestCaseSource))]
-        public void Calculate_MultipleNegativeRecord_MergeBecomeOneNegativePoint(TestCaseExpectAndActual caseDetail)
+        public void Calculate_MultipleNegativeRecord_MergeBecomeOneNegativePoint(TestCaseExpectAndActualModel caseDetail)
         {
             ///Arrange
-
+            Mock<IDateTimeService> mock_DateTimeService = new Mock<IDateTimeService>();
+            mock_DateTimeService.Setup(x => x.GetDate()).Returns(caseDetail.MockGetPointDate);
+            PointPiorityQueue _pointPiorityQueue = new PointPiorityQueue(mock_DateTimeService.Object);
+            
             ///Action
             var actual = _pointPiorityQueue.Calculate(caseDetail.funcInput);
 
@@ -80,10 +59,13 @@ namespace PointCalculator_Test
         }
         [Test]
         [TestCaseSource(typeof(MixingPositiveNegative_TestCaseSource))]
-        public void Calculate_MixingPositiveNegative_OffSet(TestCaseExpectAndActual caseDetail)
+        public void Calculate_MixingPositiveNegative_OffSet(TestCaseExpectAndActualModel caseDetail)
         {
             ///Arrange
-
+            Mock<IDateTimeService> mock_DateTimeService = new Mock<IDateTimeService>();
+            mock_DateTimeService.Setup(x => x.GetDate()).Returns(caseDetail.MockGetPointDate);
+            PointPiorityQueue _pointPiorityQueue = new PointPiorityQueue(mock_DateTimeService.Object);
+            
             ///Action
             var actual = _pointPiorityQueue.Calculate(caseDetail.funcInput);
 
